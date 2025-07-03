461a606aecd908a1e1dab3e5728d28a6e6e661eb
Aman Chourasiya <134651828+Amanmodi8293@users.noreply.github.com>
2025-07-04 00:58:01 +0530

    Update DBConnection.java

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -3,8 +3,8 @@ import java.sql.*;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/management_system";
-    private static final String USER = "Amanmodi";
-    private static final String PASSWORD = "Aman@?12";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
